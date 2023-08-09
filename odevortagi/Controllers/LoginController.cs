2e363541f1dce987db415842f383d900cf267d4a
Burhan Çavdaroğlu <burhan0664@hotmail.com>
2023-08-09 15:02:10 +0300

    Login Page front end is copy to project but now should establish back end side

---
+++ odevortagi/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Controllers
+{
+    public class LoginController:Controller
+    {
+        public IActionResult In(){
+            return View();
+        }
+        public IActionResult Up(){
+            return View();
+        }
+    }
+}
